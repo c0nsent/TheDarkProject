345b148b2334d3b5a042e4b0048f0cba98e27aac
consent_ <c0nsent@proton.me>
2026-07-19 17:53:46 +0300

    New beginning

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use beryllium::*;
-use ogl33::load_gl_with;
 
-const WINDOW_TITLE: &str = "TheDarkProject";
+const WINDOW_TITLE: &str = "The Dark Project";
 
 fn main() {
     let sdl = Sdl::init(init::InitFlags::EVERYTHING);
-    sdl.set_gl_context_major_version(3).expect("Failed to get GL version");
+
+    sdl.set_gl_context_major_version(3).unwrap();
     sdl.set_gl_context_minor_version(3).unwrap();
     sdl.set_gl_profile(video::GlProfile::Core).unwrap();
 
+    #[cfg(target_os = "macos")] {
+        sdl.set_gl_context_flags(video::GlContextFlags::FORWARD_COMPATIBLE).unwrap();
+    }
 
     let win_args = video::CreateWinArgs {
         title: WINDOW_TITLE,
@@ -19,19 +22,13 @@ fn main() {
         resizable: false,
     };
 
-    let _win = sdl
-        .create_gl_window(win_args)
-        .expect("Failed to create GL window");
-
-
-    unsafe {
-        load_gl_with(|f_name| win.get_proc_address(f_name));
-    }
+    let _win = sdl.create_gl_window(win_args).expect("Failed to create a window");
 
     'main_loop: loop {
         while let Some(event) = sdl.poll_events() {
             match event {
-                (events::Event::Quit { .. }, _) => break 'main_loop, _ => (),
+                (events::Event::Quit, _) => break 'main_loop,
+                _ => (),
             }
         }
     }
